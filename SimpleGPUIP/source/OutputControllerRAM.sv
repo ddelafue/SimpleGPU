7aa9b68244bcd22dad3a1b46e23e69e6e8535e8f
Mentor Graphics <mg57@ecegrid-thin3.ecn.purdue.edu>
2016-04-21 16:23:17 -0400

    Revert "Changes to Output Controller Ram Access"
    This reverts commit d865033eed25767d82413f5213a9ee4eacf02ed8.

--- SimpleGPUIP/source/OutputControllerRAM.sv
+++ SimpleGPUIP/source/OutputControllerRAM.sv
@@ -9,15 +9,15 @@
 
 module OutputControllerRam
 (
-	    output reg [23:0] q,
-		input [23:0] data,
+	    output reg [31:0] q,
+		input [31:0] d,
 		input [6:0] write_address, read_address,
 		input we, clk
 );
-	reg [23:0] mem [2457599:0];
+	reg [31:0] mem [2457599:0];
 	always @ (posedge clk) begin
 		if (we)
-			mem[write_address] <= data;
+			mem[write_address] <= d;
 		q <= mem[read_address]; // q doesn't get d in this clock cycle
 	end
 endmodule
