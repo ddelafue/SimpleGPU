3ee46f3f8d42dec6228d302dd155f3180dab5a57
Mentor Graphics <mg54@ecegrid-thin5.ecn.purdue.edu>
2016-04-21 17:43:30 -0400

    Adding a fifo for the input decoder

---
+++ SimpleGPUIP/source/InputDecoder_fifo_RAM.sv
@@ -0,0 +1,23 @@
+// $Id: $
+// File name:   fifo_RAM.sv
+// Created:     4/21/2016
+// Author:      Jordan Huffaker
+// Lab Section: 337-08
+// Version:     1.0  Initial Design Entry
+// Description: An abstracted version of RAM as a FIFO.
+
+module fifo_RAM
+(
+	input wire clk,
+	input wire reset,
+	input wire write,
+	input wire read,
+	input wire [31:0] r_data,
+	input wire [31:0] w_data,
+	output wire empty,
+	output wire full
+);
+
+	reg []
+
+endmodule
