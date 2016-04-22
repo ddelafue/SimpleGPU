c985d2328fdf7b898126ab4207468af1cc632a57
Mentor Graphics <mg57@ecegrid-thin3.ecn.purdue.edu>
2016-04-21 21:26:36 -0400

    Updated Output Controller

--- SimpleGPUIP/source/OutputController.sv
+++ SimpleGPUIP/source/OutputController.sv
@@ -8,6 +8,7 @@
 
 module OutputController
 (
+	input wire n_rst;	
 	//From the Alpha Blender
 	input wire [7:0] write_r,
 	input wire [7:0] write_g,
@@ -17,12 +18,12 @@ module OutputController
 	input wire frame_ready,
 
 	//From the Texture Controller
-	input wire [7:0] Pixel_Number,
+	input wire [16:0] Pixel_Number,
 
 	//Data from the M9
 	input wire [23:0] M9_rdata,
 	output wire [23:0] M9_wdata,
-	output wire [6:0] read_address, write_address,
+	output wire [16:0] read_address, write_address,
 	output wire we,
 
 
@@ -33,17 +34,100 @@ module OutputController
 	output wire [7:0] read_b,
 
 	//output to the M9. First data written, then control signals
-	output wire [31:0] M9_wdata,
-	output wire M9_read,
-	output wire M9_write,
-	output wire M9_Address,
+	output wire  M9_write,
 	//output to SD_RAM
-	output wire SD_write
-	output wire [15:0] SD_wdata,
-	output wire [31:0] SD_address,
-
+	output wire SD_write,
+	output wire [31:0] SD_wdata,
+	output wire [25:0] SD_address,
+	input wire waitrequest //wait for this in order to increment the address 
 );
 
+typedef enum bit [2:0] {M9BLEND,M9SDRAM}
+int current_MADDR = 0;
+int current_MADDW = 0;
+stateType state;
+stateType nxt_state;
+
+assign M9_write = {{write_r},{write_g},{write_b}};
+RAM m9write (.q(M9_rdata), .data(M9_wdata), .write_address(write_address), .read_address(read_address), .we(M9_write), .clk(clk));
+
+
+always_ff @ (negedge n_rst, posedge clk)
+begin
+	if(n_rst == 1'b0)
+	begin
+		state <= M9BLEND;
+	end
+	else
+	begin
+		state <= nxt_state;
+		if((write == 1'b1) && (state == M9BLEND))
+		begin
+			write_address <= write_address + 1;
+			read_address <= 0;
+		end
+		else if(read == 1'b1) && (state == M9BLEND))
+		begin
+			read_address <= Pixel_Number
+			write_address <= write_address
+		end
+			
+	end
+end 
+
+
+always_ff @ (negedge n_rst, posedge clk)
+begin
+	if(n_rst == 1'b0)
+	begin
+		state <= M9BLEND
+	end
+	else
+	begin
 	
+
+always_comb
+begin
+	nxt_state = state;
+	case(state)
+	M9BLEND:
+	begin
+		if(frame_ready == 1'b1)
+		begin
+			nxt_state = M9SDRAM
+	end
+	M9SDRAM:
+	begin
+		nxt_state = M9SDRAM
+	endcase
+end
+
+
+always_comb
+begin
+	//initialize values here. idk what these will be yet tho//
+	case(state)
+	M9BLEND:
+	begin
+		if (write == 1'b1)
+		begin
+			M9_write = 1'b1;
+		end
+		else if (read == 1'b1)
+		begin
+			M9_write = 1'b0;
+		end
+	M9SDRAM:
+	begin
+
+		
+		
+		
+		
+
+
+
+
+
 
 
