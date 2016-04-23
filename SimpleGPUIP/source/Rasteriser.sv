186bd45837f490592f68a8af5632565b782793dd
Mentor Graphics <mg59@ecegrid-thin7.ecn.purdue.edu>
2016-04-23 14:14:17 -0400

    minor changes to Rasteriser

--- SimpleGPUIP/source/Rasteriser.sv
+++ SimpleGPUIP/source/Rasteriser.sv
@@ -11,6 +11,7 @@ module Rasteriser
 	input wire clk,
 	input wire reset,
 	input wire opcode_received,
+	input wire frame_ready,
 	input wire data_ready,
 	input wire triangle_done,
 	input wire [15:0] x1,
@@ -26,7 +27,53 @@ module Rasteriser
 	output reg get_rgba,
 	output reg get_pixel,
 	output reg get_line,
-	output reg [18:0] pixel_number
+	output reg [18:0] pixel_number,
+	output wire frame_ready_o
 );
+
+typedef enum bit [2:0] {IDLE,IN_XY,GET_PIX,SEND_PIX,DONE} states;
+states state = IDLE;
+states next_state = IDLE;
+
+always_ff @ (posedge clk, negedge reset)
+begin
+	if (reset == 1'b0)
+	begin
+		state <= IDLE;
+	end
+	else
+	begin
+		state <= next_state;
+	end
+end
+
+always_comb
+begin
+	get_pixel = 1'b0;
+	next_state = IDLE;
+	case(state)
+		IDLE:
+		begin
+			if(opcode_received == 1'b1)
+			begin
+				next_state = IN_XY;
+			end
+		end
+		IN_XY:
+		begin
+			next_state = GET_PIX;
+		end
+		GET_PIX:
+		begin
+			next_state = SEND_PIX;
+			get_pixel = 1'b1;
+		end
+		SEND_PIX:
+		begin
+			get_pixel = 1'b0;
+			next_state = GET_PIX;
+		end
+	endcase
+end
 
 endmodule
--- SimpleGPUIP/source/tb_Rasteriser.sv
+++ SimpleGPUIP/source/tb_Rasteriser.sv
@@ -15,6 +15,7 @@ module tb_Rasteriser
 	reg tb_clk;
 	reg tb_reset;
 	reg tb_opcode_received;
+	reg tb_frame_ready;
 	reg tb_data_ready;
 	reg tb_triangle_done;
 	reg[15:0] tb_x1;
@@ -29,25 +30,31 @@ module tb_Rasteriser
 	wire tb_load_texture;
 	wire tb_get_rgba;
 	wire tb_get_pixel;
+	wire tb_get_line;
+	wire [18:0] tb_pixel_number;
+	wire tb_frame_ready_o;
 
 	//Connections
 	Rasteriser U1 (.clk(tb_clk),
-			.reset(tb_reset)
-			.opcode_received(tb_opcode_received)
-			.data_ready(tb_data_ready)
-			.triangle_done(tb_triangle_done)
-			.x1(tb_x1)
-			.y1(tb_y1)
-			.x2(tb_x2)
-			.y2(tb_y2)
-			.x3(tb_x3)
-			.y3(tb_y3)
-			.clear(tb_clear)
-			.count_up(tb_count_up)
-			.next_triangle(tb_next_triangle)
-			.load_texture(tb_load_texture)
-			.get_rgba(tb_get_rgba)
-			.get_pixel(tb_get_pixel));
+			.reset(tb_reset),
+			.opcode_received(tb_opcode_received),
+			.frame_ready(tb_frame_ready),
+			.data_ready(tb_data_ready),
+			.triangle_done(tb_triangle_done),
+			.x1(tb_x1),
+			.y1(tb_y1),
+			.x2(tb_x2),
+			.y2(tb_y2),
+			.x3(tb_x3),
+			.y3(tb_y3),
+			.clear(tb_clear),
+			.count_up(tb_count_up),
+			.next_triangle(tb_next_triangle),
+			.load_texture(tb_load_texture),
+			.get_rgba(tb_get_rgba),
+			.get_pixel(tb_get_pixel),
+			.pixel_number(tb_pixel_number),
+			.frame_ready_o(tb_frame_ready_o));
 
 	//Clock Signal
 	always
@@ -59,4 +66,10 @@ module tb_Rasteriser
 	begin
 		tb_clk = 1'b0;
 		tb_reset = 1'b0;
+		#4;
+		tb_opcode_received = 1'b1;
+		#4;
+		tb_opcode_received = 1'b0;
 	end
+
+endmodule
